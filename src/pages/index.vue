d64b1f32d381562f768211e1b33544399d5ad6be
贾晨昊 <s543355165@163.com>
2023-06-06 19:06:43 +0800

    feat: swiper

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,50 +1,84 @@
 <template>
   <NuxtLayout name="header" />
   <main>
-    <swiper
-      :style="{
-        '--swiper-navigation-color': '#fff',
-        '--swiper-pagination-color': '#fff',
-        '--swiper-navigation-size': '22px'
-      }"
-      :cssMode="true" :loop="true" :navigation="true" :pagination="true" :mousewheel="true" :keyboard="true"
-      :modules="modules" 
-      class="swiper"
-      @slideChange="onSlideChange"
-      >
-      <swiper-slide>
-        <img src="@/assets/images/MODERN-LUXURY-DESIGN-KITCHENS.webp" alt="澎升装饰图片">
+    <button @click="next">点击</button>
+    <SwiperComp :style="swiperStyle" :cssMode="true" :autoplay="{ delay: 2500, disableOnInteraction: false}" :loop="true" :navigation="true" :pagination="true" :mousewheel="true"
+      :keyboard="true" :modules="modules" class="swiper" ref="swiperRef" @slideChange="onSlideChange">
+      <swiper-slide v-for="(item, index) in data" :key="index" v-slot="{ isActive }">
+        <img :src="item.url" :alt="item.alt">
         <Transition>
-          <div v-if="clickedIndex === 1" class="desc">
-            <p>给我一份信赖给您一段美好</p>
+          <div v-if="isActive" class="desc">
+            <div v-html="item.text"></div>
           </div>
         </Transition>
       </swiper-slide>
-      <swiper-slide>
-        <img src="@/assets/images/MODERN-LUXURY-DESIGN-BATHROOS.webp" alt="澎升装饰图片">
-      </swiper-slide>
-      <swiper-slide> 
-        <img src="@/assets/images/MODERN-LUXURY-DESIGN-WARDROBES.webp" alt="澎升装饰图片">
-      </swiper-slide>
-    </swiper>
+    </SwiperComp>
   </main>
 </template>
 <style lang="scss" src="@/assets/styles/index.scss"></style>
-<script setup lang="ts">
-import { Swiper, SwiperSlide } from "swiper/vue";
-import type {Swiper as SwiperType} from "swiper"
+<style lang="scss" scoped>
+/* 下面我们会解释这些 class 是做什么的 */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+// .v-enter-from,
+// .v-leave-to {
+//   opacity: 0;
+// }
+</style>
+<script setup lang="tsx">
+import { Swiper as SwiperComp, SwiperSlide } from "swiper/vue";
+import type { Swiper as SwiperType } from "swiper"
 import "swiper/css";
 import "swiper/css/navigation";
 import 'swiper/css/pagination';
+import Swipers from 'swiper'
 
-import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import { Autoplay, Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 
-const modules = [Navigation, Pagination, Mousewheel, Keyboard];
+import img1 from "@/assets/images/MODERN-LUXURY-DESIGN-KITCHENS.webp"
+import img2 from "@/assets/images/MODERN-LUXURY-DESIGN-BATHROOS.webp"
+import img3 from "@/assets/images/MODERN-LUXURY-DESIGN-WARDROBES.webp"
 
-const clickedIndex = ref(1)
+const modules = [Autoplay, Navigation, Pagination, Mousewheel, Keyboard];
+
+const swiperStyle = {
+  '--swiper-navigation-color': '#fff',
+  '--swiper-pagination-color': '#fff',
+  '--swiper-navigation-size': '22px'
+}
+
+const data = [{
+  url: img1,
+  text: `<p>给我一份信赖给您一段美好</p>
+    <p>澎升, 您身边的全屋定制</p>`,
+  alt: '澎升装饰图片'
+},
+{
+  url: img2,
+  text: `<p>给我一份信赖给您一段美好</p>
+    <p>澎升, 您身边的全屋定制</p>`,
+    alt: '澎升装饰图片'
+}, {
+  url: img3,
+  text: `<p>给我一份信赖给您一段美好</p>
+    <p>澎升, 您身边的全屋定制</p>`,
+    alt: '澎升装饰图片'
+}]
+
+const swiperRef = ref()
+const swiper = ref()
+
+const clickedIndex = ref(0)
 const onSlideChange = (e: SwiperType) => {
-  clickedIndex.value = e.clickedIndex
-  console.log(`(╯‵□′)╯︵┻━┻`, e.clickedIndex)
+  clickedIndex.value = e.activeIndex
+}
+
+const next = () => {
+  console.log(`(╯‵□′)╯︵┻━┻`, swiper.value)
+  // swiper.value.autoplay.stop()
 }
 
 const router = useRouter();
